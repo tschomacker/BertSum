5e7b20f0704464f760a60b9e9f0215312876a2a4
Santosh Gupta <Santosh.gupta.eng@gmail.com>
2019-10-05 22:33:17 -0700

    Update train.py

--- src/train.py
+++ src/train.py
@@ -261,7 +261,7 @@ def testToCSVNewData(args, device_id, pt, step):
     model.load_cp(checkpoint)
     model.eval()
 
-    test_iter =data_loader.Dataloader(args, load_dataset(args, 'test', shuffle=False),
+    test_iter =data_loader.Dataloader(args, load_datasetToCSVNewData(args, 'test', shuffle=False),
                                   args.batch_size, device,
                                   shuffle=False, is_test=True)
     trainer = build_trainer(args, device_id, model, None)
