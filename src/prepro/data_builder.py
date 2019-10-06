55ac295b21f524efbd671d528249d022400fca84
Santosh Gupta <Santosh.gupta.eng@gmail.com>
2019-10-05 21:43:13 -0700

    Update data_builder.py

--- src/prepro/data_builder.py
+++ src/prepro/data_builder.py
@@ -216,6 +216,24 @@ def format_to_bert(args):
 
         pool.close()
         pool.join()
+        
+def format_to_bertMS(args):
+    if (args.dataset != ''):
+        datasets = [args.dataset]
+    else:
+        datasets = ['train', 'valid', 'test']
+    for corpus_type in datasets:
+        a_lst = []
+        for json_f in glob.glob(pjoin(args.raw_path, '*' + corpus_type + '.json')):
+            real_name = json_f.split('/')[-1]
+            a_lst.append((json_f, args, pjoin(args.save_path, real_name.replace('json', 'bert.pt'))))
+        print(a_lst)
+        pool = Pool(args.n_cpus)
+        for d in pool.imap(_format_to_bert, a_lst):
+            pass
+
+        pool.close()
+        pool.join()
 
 
 def tokenize(args):
